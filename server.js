fc701bc408d128b343ef041a2e6ffec223d6394c
Ian Stock <i_stock@hotmail.com>
2018-09-10 13:30:19 +0100

    first version of the app, talking from client to server, next step to iot

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
 app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 8080, () => console.log('node app running'))
+
+
+app.post('/api', function(req, res){
+    //console.log('body: ' + JSON.stringify(req.body));
+	res.send(req.body);
+});
