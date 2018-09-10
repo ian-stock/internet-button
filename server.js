c683b3931b74e8ef37e1a82985deb4fc18863659
Ian Stock <i_stock@hotmail.com>
2018-09-10 16:32:23 +0100

    fully linked up to iot

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const request = require('request')
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
@@ -9,6 +10,21 @@ app.listen(process.env.PORT || 8080, () => console.log('node app running'))
 
 
 app.post('/api', function(req, res){
+    var msurl = 'http://particlephotonintegration-wyct.us-e2.cloudhub.io/eventpub';
+    request({
+        url: msurl,
+        method: "POST",
+        json: req.body
+    }, function (error, response, body){
+        if (!error && response.statusCode === 200) {
+            console.log(body)
+        }
+        else {
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+        }
+    });
     //console.log('body: ' + JSON.stringify(req.body));
-	res.send(req.body);
+    res.send(req.body);
 });
